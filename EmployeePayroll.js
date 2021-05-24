bddf3ce2662e1fd5544293e60e4bdcbf338c5f99
shaheen5 <miyashaheen1995@gmail.com>
2021-05-24 09:02:43 +0530

    Added : Code To Create Employee Payroll Data Objects

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,1 +1,28 @@
-console.log("Welcome to Employee Payroll Problem");
+class EmployeePayrollData {
+    //property
+    id;
+    name;
+    salary;
+
+    //constructor
+    constructor(id,name,salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    //getter and setter method
+    get name(){ return this.name;}
+    set name(name) {
+        this.name = name;
+    }
+
+    toString(){
+        return "id="+this.id+", name="+this.name+", salary="+
+        this.salary;
+    }
+}
+let employeePayrollData = new EmployeePayrollData(1,"Mark",300000);
+console.log(employeePayrollData);
+employeePayrollData.name="Shaheen";
+console.log(employeePayrollData);
