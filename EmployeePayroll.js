426ffc5a3154c1da7c7e8cdaf1203d043c4a0786
shaheen5 <miyashaheen1995@gmail.com>
2021-05-24 09:20:01 +0530

    Modified : Code To Add Gender And StartDate To Payroll

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -3,12 +3,16 @@ class EmployeePayrollData {
     id;
     name;
     salary;
+    gender;
+    startDate;
 
     //constructor
-    constructor(id,name,salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     //getter and setter method
@@ -18,11 +22,16 @@ class EmployeePayrollData {
     }
 
     toString(){
+        const options = { year:'numeric',month:'long',day:'long'}
+        const empDate = this.startDate === undefined ? "undefined" :
+                        this.startDate.toLocaleDateString("en-US",options);
         return "id="+this.id+", name="+this.name+", salary="+
-        this.salary;
+        this.salary+", gender="+this.gender+", startDate="+this.startDate;
     }
 }
 let employeePayrollData = new EmployeePayrollData(1,"Mark",300000);
 console.log(employeePayrollData);
 employeePayrollData.name="Shaheen";
 console.log(employeePayrollData);
+let newEmployeePayrollData = new EmployeePayrollData(2,"John",400000,'M',new Date());
+console.log(newEmployeePayrollData);
